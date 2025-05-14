c59c152eb46efdd43d3b7025d1711d1efb005412
Nguyễn Gia Bảo <ngbaodev@gmail.com>
2025-05-15 01:50:39 +0700

    feat: implement footer page (#14)

---
+++ src/components/ui/svg-icon.tsx
@@ -0,0 +1,26 @@
+import React, { ReactElement, SVGProps } from 'react';
+
+interface SvgIconType extends SVGProps<SVGSVGElement> {
+  size?: number | string;
+  color?: string;
+  className?: string;
+  children: ReactElement<SVGProps<SVGSVGElement>>;
+}
+
+export const SvgIcon = ({
+  size = 24,
+  color = 'currentColor',
+  children,
+  ...props
+}: SvgIconType) => {
+  console.log('----------> color', color);
+  if (!children) return null;
+  return React.cloneElement(children, {
+    width: size,
+    height: size,
+    fill: children.props.fill ?? color,
+    ...props,
+  });
+};
+
+export type SvgIconProps = Omit<SvgIconType, 'children'>;
