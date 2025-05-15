8b437b8f8a132e44bc3e3f6cb92ba16a05063c77
Nguyễn Gia Bảo <ngbaodev@gmail.com>
2025-05-15 16:54:48 +0700

    feat: implement business key component (#20)

---
+++ src/app/(base)/_components/business-key.tsx
@@ -0,0 +1,65 @@
+import {
+  EcoFriendlyIcon,
+  SustainablePackIcon,
+  UsefulDesignIcon,
+} from '@/components/icons';
+import { HealthyProductIcon } from '@/components/icons/healthy-product';
+import { Flex } from '@/components/ui/flex';
+import { SvgIconProps } from '@/components/ui/svg-icon';
+import { cn } from '@/lib/utils';
+import { JSX, ReactElement } from 'react';
+
+interface BusinessKeyContent {
+  icon: (props: SvgIconProps) => JSX.Element;
+  description: ReactElement;
+}
+
+const textStyle = cn('text-center', 'text-base', 'font-medium', 'text-arsenic');
+
+const BUSINESS_KEY_CONTENT: BusinessKeyContent[] = [
+  {
+    icon: HealthyProductIcon,
+    description: (
+      <p className={textStyle}>
+        Healthy <br /> Products
+      </p>
+    ),
+  },
+  {
+    icon: UsefulDesignIcon,
+    description: (
+      <p className={textStyle}>
+        Useful <br /> Designs
+      </p>
+    ),
+  },
+  {
+    icon: EcoFriendlyIcon,
+    description: (
+      <p className={textStyle}>
+        Eco-friendly <br /> Manufacturing
+      </p>
+    ),
+  },
+  {
+    icon: SustainablePackIcon,
+    description: (
+      <p className={textStyle}>
+        Sustainable <br /> Packaging
+      </p>
+    ),
+  },
+];
+
+export function BusinessKey() {
+  return (
+    <Flex className="flex-col md:flex-row mx-auto justify-between items-center py-18 gap-6">
+      {BUSINESS_KEY_CONTENT.map((item, index) => (
+        <Flex key={index} className="flex-col items-center gap-4 min-w-[182px]">
+          <item.icon size={96} />
+          {item.description}
+        </Flex>
+      ))}
+    </Flex>
+  );
+}
